f8fb24946d19f4fc703cbb1f8cb58d75844ff338
Miten Soni <msoni099@uottawa.ca>
2024-01-30 06:34:56 -0500

    lab3 setup, breadcrumbs, styling

---
+++ Labs/Lab3/scripts/groceries.js
@@ -0,0 +1,114 @@
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products
+
+var products = [
+  {
+    id: "chicken",
+    name: "Chicken",
+    price: 15.5,
+    diet: ["GF", "ORG", "DB", "LF"],
+  },
+  {
+    id: "avocado",
+    name: "Avocado",
+    price: 3.25,
+    diet: ["VEG", "GF", "DB", "LF"],
+  },
+  {
+    id: "bread",
+    name: "Bread",
+    price: 4.5,
+    diet: ["VEG", "ORG", "LF"],
+  },
+  {
+    id: "rice",
+    name: "Rice",
+    price: 3.5,
+    diet: ["VEG", "GF", "LF"],
+  },
+  {
+    id: "bacon",
+    name: "Bacon",
+    price: 5.0,
+    diet: ["GF", "DB", "LF"],
+  },
+  {
+    id: "steak",
+    name: "Steak",
+    price: 30.0,
+    diet: ["GF", "OR", "DB", "LF"],
+  },
+  {
+    id: "broccoli",
+    name: "Broccoli",
+    price: 2.5,
+    diet: ["VEG", "GF", "OR", "DB", "LF"],
+  },
+  {
+    id: "banana",
+    name: "Banana",
+    price: 1.25,
+    diet: ["VEG", "GF", "OR", "DB", "LF"],
+  },
+  {
+    id: "crab",
+    name: "Crab",
+    price: 20.5,
+    diet: ["GF", "OR", "DB", "LF"],
+  },
+  {
+    id: "watermelon",
+    name: "Watermelon",
+    price: 5.0,
+    diet: ["VEG", "GF", "OR", "LF"],
+  },
+  {
+    id: "milk",
+    name: "Milk",
+    price: 5.0,
+    diet: ["VEG", "GF", "OR"],
+  },
+  {
+    id: "oatmilk",
+    name: "Oatmilk",
+    price: 7.0,
+    diet: ["VEG", "GF", "OR", "LF"],
+  },
+];
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, restrictions) {
+  let product_info = [];
+
+  if (restrictions[0] == "NONE") {
+    return prods;
+  }
+
+  for (let i = 0; i < prods.length; i += 1) {
+    var flag = true;
+    for (let restriction of restrictions) {
+      if (!prods[i].diet.includes(restriction)) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag == true) {
+      product_info.push(prods[i]);
+    }
+  }
+
+  return product_info;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+  totalPrice = 0;
+  for (let i = 0; i < products.length; i += 1) {
+    if (chosenProducts.indexOf(products[i].name) > -1) {
+      totalPrice += products[i].price;
+    }
+  }
+  return totalPrice;
+}
---
+++ Labs/Lab3/scripts/main.js
@@ -0,0 +1,186 @@
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+let cart = [];
+
+function openInfo(evt, tabName) {
+  console.log(evt);
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// This function disables other options if the 'None' option is selected
+function handleNoneCheckbox(checkbox) {
+  var otherCheckboxes = document.querySelectorAll(
+    'input[name="diet"]:not([value="NONE"])'
+  );
+  for (var i = 0; i < otherCheckboxes.length; i++) {
+    otherCheckboxes[i].disabled = checkbox.checked;
+    if (checkbox.checked) {
+      otherCheckboxes[i].checked = false;
+    }
+  }
+}
+
+// handles the clicking of 'View Products' and displays the relevant products
+function submitDietRestrictions(slct2) {
+  var selectedOptions = [];
+  var checkboxes = document.getElementsByName("diet");
+  var s2 = document.getElementById(slct2);
+  // s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+  s2.innerHTML = "";
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      selectedOptions.push(checkboxes[i].value);
+    }
+  }
+
+  // obtain a reduced list of products based on restrictions and render them
+  var optionArray = restrictListProducts(products, selectedOptions);
+
+  for (i = 0; i < optionArray.length; i++) {
+    var product = optionArray[i];
+    // new code
+    // create product div and add it in the DOM
+    var productContainer = createProductDiv(product);
+    s2.appendChild(productContainer);
+  }
+
+  // Switch to the "Products" tab
+  productTablink = document.getElementsByClassName("tablinks products");
+  productTablink.className += " active";
+  openInfo(event, "Products");
+}
+
+// Building each product tile
+function createProductDiv(product) {
+  const productContainer = document.createElement("div");
+  productContainer.id = product.id;
+  productContainer.classList.add("product-container");
+
+  const productImage = document.createElement("img");
+  productImage.classList.add("product-image");
+  productImage.src = `assets/${product.id}.png`;
+  productImage.alt = `${product.name} (whole)`;
+  productContainer.appendChild(productImage);
+
+  const productDesc = document.createElement("div");
+  productDesc.classList.add("product-desc");
+  const productName = document.createElement("h2");
+  productName.classList.add("product-title");
+  productName.textContent = product.name;
+  productDesc.appendChild(productName); // name
+
+  const productPrice = document.createElement("span");
+  productPrice.classList.add("price");
+  productPrice.textContent = `$${product.price.toFixed(2)}`;
+  productDesc.appendChild(productPrice); // price
+
+  const addDiv = document.createElement("div");
+  addDiv.classList.add("add-quantity-box");
+  const addToCartButton = document.createElement("button");
+  addToCartButton.classList.add("add-to-cart-button");
+  addToCartButton.textContent = "Add to Cart";
+
+  addDiv.appendChild(addToCartButton);
+
+  const quantityDropdown = document.createElement("select");
+  quantityDropdown.classList.add("quantity-dropdown");
+  for (var i = 1; i <= 5; i++) {
+    var opt = document.createElement("option");
+    opt.value = i;
+    opt.innerHTML = i;
+    quantityDropdown.appendChild(opt);
+  }
+  quantityDropdown.onclick = function () {};
+  addDiv.appendChild(quantityDropdown);
+  productDesc.appendChild(addDiv);
+  productContainer.appendChild(productDesc);
+  addToCartButton.onclick = function () {
+    const selectedQuantity = quantityDropdown.value;
+    toggleCartStatus(this, product, selectedQuantity);
+  };
+
+  return productContainer;
+}
+
+// handles the clicking of 'Add to Cart'
+function toggleCartStatus(button, product, quantity) {
+  var notification = document.getElementById("notification");
+  console.log(product);
+
+  if (button.innerText === "Add to Cart") {
+    cart.push([product, quantity]);
+    notification.innerText = `Added to cart!`;
+    button.innerText = "Remove from Cart";
+    button.classList.add("in-cart");
+  } else {
+    const index = cart.findIndex(([item, quantity]) => item.id === product.id);
+    if (index !== -1) {
+      cart.splice(index, 1);
+    }
+    notification.innerText = "Item removed from cart!";
+    button.innerText = "Add to Cart";
+    button.classList.remove("in-cart");
+  }
+  populateCart(cart);
+
+  notification.style.display = "block";
+  setTimeout(function () {
+    notification.style.display = "none";
+  }, 2500);
+}
+
+function populateCart(cart) {
+  const cartContainer = document.getElementById("Cart");
+  cartContainer.innerHTML = "";
+  subtotal = 0;
+
+  if (cart.length === 0) {
+    cartContainer.innerHTML = "<p>Your cart is empty.</p>";
+  } else {
+    const productList = document.createElement("ul");
+    cart.forEach(function ([product, quantity]) {
+      const listItem = createProductListItem(product, quantity);
+      productList.appendChild(listItem);
+    });
+    cartContainer.appendChild(productList);
+    cart.forEach(function ([product, quantity]) {
+      subtotal += product.price * quantity;
+    });
+    const subtotalElement = document.createElement("p");
+    subtotalElement.textContent = `Total: $${subtotal.toFixed(2)}`;
+    cartContainer.appendChild(subtotalElement);
+  }
+}
+
+function createProductListItem(product, quantity) {
+  const listItem = document.createElement("li");
+  const productName = document.createElement("span");
+  productName.textContent = product.name;
+  listItem.appendChild(productName);
+  const quantityAndPrice = document.createElement("span");
+  listItem.appendChild(quantityAndPrice);
+  quantityAndPrice.textContent = quantity + " x $" + product.price;
+  const productPrice = document.createElement("span");
+  productPrice.textContent = `$${(product.price * quantity).toFixed(2)}`;
+  listItem.appendChild(productPrice);
+  return listItem;
+}
+
+function sortByPrice(itemA, itemB) {
+  return itemA.price - itemB.price;
+}
