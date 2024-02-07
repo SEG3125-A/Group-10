cca79e7a12a912b2ee5a310523a5fc10a4eec184
Antoine Lavigne <63273266+antoine2304@users.noreply.github.com>
2024-02-07 17:36:32 -0500

    Add the service select page

---
+++ Labs/Lab4/scripts/main.js
@@ -0,0 +1,33 @@
+// List of pages for navigation
+const pages = [{
+    id: 'Service-Select',
+    title: 'Services',
+}, {
+    id: 'Time-Select',
+    title: 'Select time of appointment',
+}];
+
+function navigateTo(pageId) {
+    const page = pages.find(p => p.id === pageId);
+    if (!page) {
+        console.error(`Page with id ${pageId} not found`);
+        return;
+    }
+    console.log(`Navigating to page: ${page.title}`);
+
+    // Hide all pages
+    pages.forEach(p => {
+        const pageElement = document.getElementById(p.id);
+        if (pageElement) {
+            pageElement.style.display = 'none';
+        }
+    });
+
+    // Show the selected page
+    const pageElement = document.getElementById(pageId);
+    if (pageElement) {
+        pageElement.style.display = 'block';
+    } else {
+        console.error(`Page with id ${pageId} not found`);
+    }
+}
