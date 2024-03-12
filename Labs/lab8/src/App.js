faabe6edcd7978d9b0f918c0cc41e9d3b0779094
antoine2304 <antoinelavigne04@gmail.com>
2024-03-12 14:10:04 -0400

    Home page

--- Labs/lab8/src/App.js
+++ Labs/lab8/src/App.js
@@ -1,8 +1,17 @@
+import { Button } from '@mui/material';
+
 function App() {
   return (
-    <div className="App">
-      <text>Hello World!</text>
+    <div>
+      <img src={require("./background.png")} style={{width: "100%", height: "100%", position: "absolute", left: 0, top: 0, zIndex: -100}}/>
+      <header style={{fontFamily: "NonSans", fontSize: 80, textAlign: "center", bottom: "50%", position: "absolute", width: "100%"}}>CosmetiGo</header>
+      <div style={{position: "absolute", height: 60, width: "100%", top:0, marginTop: 10, display: "flex", flexDirection: "row"}}>
+        {/** Header */}
+        <Button style={{height: 40, backgroundColor: "white", color: "black", margin: 10}}>Services</Button>
+        <Button style={{height: 40, backgroundColor: "white", color: "black", margin: 10}}>Professionals</Button>
+      </div>
     </div>
+    
   );
 }
 
