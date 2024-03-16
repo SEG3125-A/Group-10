5b1ba9ff25226fd23cbdb65da83612f2a519d367
Mohamad Cheaito <mchea082@uottawa.ca>
2024-03-16 13:09:21 -0400

    home page

--- Labs/lab8/src/Pages/Home.js
+++ Labs/lab8/src/Pages/Home.js
@@ -1,17 +1,19 @@
-import { Button } from '@mui/material';
+import React from 'react';
+import { Button, Card } from '@mui/material';
 
 function Home() {
   return (
-    <div>
-      <img src={require("../images/background.png")} style={{width: "100%", height: "100%", position: "absolute", left: 0, top: 0, zIndex: -100}}/>
-      <header style={{fontFamily: "NonSans", fontSize: 80, textAlign: "center", bottom: "50%", position: "absolute", width: "100%"}}>CosmetiGo</header>
-      <Button style={{height: 40, backgroundColor: "white", color: "black", margin: 10, marginLeft: 'auto', position: "absolute", bottom: "40%", left: "45%"}} onClick={() => {
-        // Navigate to /services
-        window.location.href = "/services";
-      }
-      }>Get Started</Button>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', position: 'relative' }}>
+      <img src={require('../images/background.png')} alt="Background" style={{ width: '100%', height: '100%', position: 'absolute', left: 0, top: 0, zIndex: -1 }} />
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Card style={{ margin: 10, height:"40vh", padding: 20, width: "50vw", borderRadius: 20, backgroundColor: '#f5f5f5', textAlign: 'center' }}>
+            <header style={{ fontSize: 50, margin: 50, marginBottom:80}}>CosmetiGo</header>
+            <Button style={{ height: "10vh",border:"3px solid", width:"20vw",fontWeight:"bolder",fontSize:"larger", backgroundColor: '#e1f1fd', color: 'black'}} onClick={() => (window.location.href = '/services')}>
+              Get Started
+            </Button>
+        </Card>
+      </div>
     </div>
-    
   );
 }
 
